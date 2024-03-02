e06776b4e2d13c4aa3677fda42de32ebe7082aaf
Armen <mirzoyana805@gmail.com>
2024-03-02 14:49:45 +0400

    First Commit : Kafka Producer, Consumer , Topic is created. PostgresSql Kafka included with docker-compose, The problem with Kafka Deserialization was solved

--- src/main/java/com/example/KafkaSpringdb/consumer/KafkaConsumer.java
+++ src/main/java/com/example/KafkaSpringdb/consumer/KafkaConsumer.java
@@ -1,7 +1,6 @@
 package com.example.KafkaSpringdb.consumer;
 
 import com.example.KafkaSpringdb.model.User;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
